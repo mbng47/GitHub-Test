57c256d1f2bf59f8b4a5413c2e17a0353005eb9a
Martin <102648195+mbng47@users.noreply.github.com>
2023-01-13 09:29:53 +0100

    random update
    hejhej

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+let frukter = ["äpple" , "päron" , "apelsin"];
+
+frukter.push("Citron");
+
+console.log(Math.ceil(Math.random()*5));
+
+/*
+let odds = getChance( 0.5 , true);
+
+getChance();
+*/
+
+let k = 3;
+
+k += 2;
+
+// x *= 2;
+
+for(let count = 5 ; count >= 1 ; count -= 1){
+    console.log("countdown " + count)
+}
+
+
+let count = 5;
+while(count >= 1){
+    console.log("countdown " + count);
+    count -= 1;
+}
+
+let x = 0.5 , y = "hello" , z = 6
+
+y = y+1
+
+
+
+let text = "Marco";
+let text2 = " Polo";
+
+// text += text2;
+
+text2 = text.concat("Marco");
+
+// text = text + text2;
+
+
+let store = {
+    name : "ica" ,
+    openingHours : "08:00-20:00" ,
+    isOpened : true ,
+    rentPerMonth : 22000
+    };
+
+
+// store.openingHours = "08:00-21:00";
+
+// store["openingHours"] = "08:00 - 22:00";
+
+
+
+
+let odds = getChance( 0.5 , true);
+
+getChance();
